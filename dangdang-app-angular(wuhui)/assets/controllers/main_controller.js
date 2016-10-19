e0399796ad8d79d6449ab861772fe8b84d2a8122
cjwddyr <497528326@qq.com>
2016-10-19 16:21:22 +0800

    还不可以吗？

--- dangdang-app-angular(wuhui)/assets/controllers/main_controller.js
+++ dangdang-app-angular(wuhui)/assets/controllers/main_controller.js
@@ -2,4 +2,10 @@ app.controller('mainController', ['$scope', 'commonService', function($scope, c_
     c_service.getData('ertong', function(res){
         console.dir(res);
     });
+
+    // 设置底部导航的展示效果。
+    $scope.selectedIndex = 0;
+    $scope.linkTo = function(index){
+        $scope.selectedIndex = index;
+    }
 }])
