54799fd3402bf0e6573042d820f2d525477c9891
cjwddyr <497528326@qq.com>
2016-10-25 10:20:22 +0800

    模板的框架导入

--- dangdang-app-angular(wuhui)/assets/controllers/book_list_controller.js
+++ dangdang-app-angular(wuhui)/assets/controllers/book_list_controller.js
@@ -1,4 +1,5 @@
 app.controller('bookListController', ['$scope', '$routeParams', 'commonService', function($scope, $routeParams, c_s){
+    $scope.data = [];
     c_s.getData($routeParams.id, function(res){
         $scope.data = res.data;
     });
